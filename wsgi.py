ae3beaf5075f9040717a6694e164466b15e2dfb6
Humbertzhang <504490160@qq.com>
2019-04-07 11:23:32 +0800

    change port

--- wsgi.py
+++ wsgi.py
@@ -6,4 +6,4 @@ def chart_file():
     return render_template('list.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='3000')
+    app.run(debug=True, host='0.0.0.0', port='2000')
